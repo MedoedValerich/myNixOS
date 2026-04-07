888518cfd3b60c1e1e49be0e05dca2a25a1bdb5b
MedoedValerich <x.medoed.x@yandex.ru>
2026-04-07 08:15:02 +0700

    Add files via upload

---
+++ myNixOS/modules/hosts/my-machine/hardware.nix
@@ -0,0 +1,25 @@
+{ self, inputs, ... }: {
+
+  flake.nixosModules.myMachineHardware = { config, lib, pkgs, modulesPath, ... }:
+
+{
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/a7f80159-2706-4acb-99b0-3b5a689170fe";
+      fsType = "ext4";
+    };
+
+  swapDevices = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+};
+}
